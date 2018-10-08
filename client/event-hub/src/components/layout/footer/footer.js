e8a6d92eca3a6d80bb035adab36c58c48f59f373
Lihong Han <littlefly14@gmail.com>
2018-10-08 18:30:41 -0500

    add emotion to style elements

--- client/event-hub/src/components/layout/footer/footer.js
+++ client/event-hub/src/components/layout/footer/footer.js
@@ -1,8 +1,12 @@
 import React from "react";
-import classes from "./footer.css";
+import { css } from "emotion";
 
+const Footer = css`
+  background-color: red;
+  border: 1px solid green;
+`;
 const footer = props => {
-  return <div className={classes.Footer}>haha</div>;
+  return <div className={Footer}>haha</div>;
 };
 
 export default footer;
