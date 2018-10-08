b4eac2430f3ce9f2a1dfdcd0441fbbc49840a90d
Lihong Han <littlefly14@gmail.com>
2018-10-08 15:41:50 -0500

    change css config

--- client/event-hub/src/components/layout/header/header.js
+++ client/event-hub/src/components/layout/header/header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import classes from "./header.css";
 
 const header = props => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <Typography variant="title" color="inherit">
-          <a href="#">Eventhub</a>
+        <Typography
+          variant="title"
+          color="inherit"
+          className={classes.MainLogo}
+        >
+          <a href="/" active>
+            Eventhub
+          </a>
         </Typography>
       </Toolbar>
     </AppBar>
