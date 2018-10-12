723aeb189f9ac134920381a2c1e8df655f6c0789
Lihong Han <littlefly14@gmail.com>
2018-10-11 19:28:39 -0500

    fix nest route bug

--- client/event-hub/src/components/LogIn/form/start.js
+++ client/event-hub/src/components/LogIn/form/start.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from "react";
 import { TextField, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 const styles = theme => ({
+  textField: {
+    width: "300px"
+  },
   button: {
-    backgroundColor: "#ff3333"
+    backgroundColor: "#ff3333",
+    width: "300px"
+  },
+  toolbarItem: {
+    textDecoration: "none",
+    color: "white"
   }
 });
 const start = props => {
+  let SignUpOrLogIn = "";
+  if (true) {
+    SignUpOrLogIn = "LogIn";
+  } else {
+    SignUpOrLogIn = "SignUp";
+  }
   const { classes } = props;
   return (
     <Fragment>
@@ -23,7 +38,9 @@ const start = props => {
       />
       <br />
       <Button variant="contained" color="secondary" className={classes.button}>
-        Get Started
+        <Link to={`/SignIn/${SignUpOrLogIn}`} className={classes.toolbarItem}>
+          Get Started
+        </Link>
       </Button>
     </Fragment>
   );
